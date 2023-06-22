6609b0c71cba9ac3fba1b163963e46ac709baad8
Lj7nr <ibe.ifeanyi997@gmail.com>
2023-06-22 04:11:12 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,14 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	int lowercase = 'a';
-	while (lowercase <= 'z')
-	{
-		putchar(lowercase);
-		lowercase += 1;
-	}
-	putchar('\n');
-	return (0);
+int lowercase = 'a';
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase += 1;
 }
-
+putchar('\n');
+return (0);
+}
